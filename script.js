2e55da6b2f2f718ba3c078615aa4b29179d5305b
Andre <ajsmoothdre@gmail.com>
2019-04-30 20:57:02 -0400

    Javascript Lab 2

--- script.js
+++ script.js
@@ -6,34 +6,52 @@ let userWins = 0;
 let grantsWins = 0;
 let grantsDamage = 0;
 let userDamage = 0;
+let userChoice;
+let name;
 
-if (play === "yes") {
-    let name = prompt("What is your name?");
-    console.log("The user said yes");
+function startGame () {
+    if (play === "yes") {
+        name = prompt("Enter your character name");
+        console.log("The user said yes");
+        startCombat();
+    } else {
+        return;
+    }
+}
+
+     function startCombat () {
     while (userWins < 3 && grantsWins < 1) {
-        let userDamage = Math.floor((Math.random() * 2) + 1);
-        let grantsDamage = Math.floor((Math.random() * 2) + 1);
-        userHealth = userHealth - userDamage;
-        grantsHealth = grantsHealth - grantsDamage;
-        console.log(`${name} has ${userHealth} health left.`);
-        console.log(`Grant has ${grantsHealth} health left.`);
+        userChoice = prompt("Would you like to attack or quit?")
+        if (userChoice === "attack") {
+            getDamage();
+            userHealth = userHealth - userDamage;
+            grantsHealth = grantsHealth - grantsDamage;
+            console.log(`${name} has ${userHealth} health left.`);
+            console.log(`Grant has ${grantsHealth} health left.`);
 
-        if (grantsHealth <= 0) {
-            userWins++;
-            grantsHealth = 10;
-            console.log(`user has ${userWins} wins`)
-        } else if (userHealth <= 0) {
-            grantsWins++;
-            console.log("Grant wins! You lose!");
-            break;
-        }
-        if (userWins === 3) {
-            console.log("You win!")
-            break;
-        }
+            if (grantsHealth <= 0) {
+                userWins++;
+                grantsHealth = 10;
+                console.log(`user has ${userWins} wins`)
+            } else if (userHealth <= 0) {
+                grantsWins++;
+                console.log("Grant wins! You lose!");
+                break;
+            }
+            if (userWins === 3) {
+                console.log("You win!")
+                break;
+            }
+        } else {
+            return;
+
+    }
+} 
+ 
+    function getDamage () {
+            userDamage = Math.floor((Math.random() * 5) + 1);
+            grantsDamage = Math.floor((Math.random() * 5) + 1);
     }
 
 }
-else {
-    console.log("See you later");
-}
+    startGame();
